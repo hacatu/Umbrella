615d7206df79bd896cae9f211d18860ab0987036
hacatu <hacatu5000@gmail.com>
2014-12-13 23:52:20 -0500

    Added parsers
    identifier
    operator
    integer
    literal
    string
    char
    tuple
    dict
    op_c

--- parser.c
+++ parser.c
@@ -46,6 +46,8 @@ literal=
 	char
 	tuple
 	dict
+*/
+/*
 start=
 	statements
 statements=
@@ -114,17 +116,18 @@ int space(PRA_Position *p, PRA_Ptree *t);//
 int whitespace_char(PRA_Position *p, PRA_Ptree *t);//
 int indent(PRA_Position *p, PRA_Ptree *t);
 int dedent(PRA_Position *p, PRA_Ptree *t);
-int identifier(PRA_Position *p, PRA_Ptree *t);
-int operator(PRA_Position *p, PRA_Ptree *t);
-int operatorBesidesSpace(PRA_Position *p, PRA_Ptree *t);
-int sigils(PRA_Position *p, PRA_Ptree *t);
-int sigil(PRA_Position *p, PRA_Ptree *t);
+int identifier(PRA_Position *p, PRA_Ptree *t);//
+int identifier_char(PRA_Position *p, PRA_Ptree *t);//
+int operator(PRA_Position *p, PRA_Ptree *t);//
+int operatorBesidesSpace(PRA_Position *p, PRA_Ptree *t);//
+int sigils(PRA_Position *p, PRA_Ptree *t);//
+int sigil(PRA_Position *p, PRA_Ptree *t);//
 int integer(PRA_Position *p, PRA_Ptree *t);//
-int int_2(PRA_Position *p, PRA_Ptree *t);
-int int_8(PRA_Position *p, PRA_Ptree *t);
+int int_2(PRA_Position *p, PRA_Ptree *t);//
+int int_8(PRA_Position *p, PRA_Ptree *t);//
 int int_10(PRA_Position *p, PRA_Ptree *t);//
-int int_16(PRA_Position *p, PRA_Ptree *t);
-int int_64(PRA_Position *p, PRA_Ptree *t);
+int int_16(PRA_Position *p, PRA_Ptree *t);//
+int int_64(PRA_Position *p, PRA_Ptree *t);//
 int digit_2(PRA_Position *p, PRA_Ptree *t);//
 int digit_8(PRA_Position *p, PRA_Ptree *t);//
 int digit_10(PRA_Position *p, PRA_Ptree *t);//
@@ -132,18 +135,19 @@ int digit_16(PRA_Position *p, PRA_Ptree *t);//
 int digit_64(PRA_Position *p, PRA_Ptree *t);//
 int decimal(PRA_Position *p, PRA_Ptree *t);//
 int decimal_sub(PRA_Position *p, PRA_Ptree *t);//
-int literal(PRA_Position *p, PRA_Ptree *t);
-int string(PRA_Position *p, PRA_Ptree *t);
-int character(PRA_Position *p, PRA_Ptree *t);
-int tuple(PRA_Position *p, PRA_Ptree *t);
-int dict(PRA_Position *p, PRA_Ptree *t);
+int literal(PRA_Position *p, PRA_Ptree *t);//
+int string(PRA_Position *p, PRA_Ptree *t);//
+int string_char(PRA_Position *p, PRA_Ptree *t);//
+int character(PRA_Position *p, PRA_Ptree *t);//
+int tuple(PRA_Position *p, PRA_Ptree *t);//
+int dict(PRA_Position *p, PRA_Ptree *t);//
 int start(PRA_Position *p, PRA_Ptree *t);
 int statements(PRA_Position *p, PRA_Ptree *t);
 int statement(PRA_Position *p, PRA_Ptree *t);
 int newline(PRA_Position *p, PRA_Ptree *t);
 int block(PRA_Position *p, PRA_Ptree *t);
 int expr_c(PRA_Position *p, PRA_Ptree *t);
-int op_c(PRA_Position *p, PRA_Ptree *t);
+int op_c(PRA_Position *p, PRA_Ptree *t);//
 int l_expr(PRA_Position *p, PRA_Ptree *t);
 int l_expr_3(PRA_Position *p, PRA_Ptree *t);
 int l_expr_1(PRA_Position *p, PRA_Ptree *t);
@@ -155,18 +159,79 @@ int r_expr(PRA_Position *p, PRA_Ptree *t);
 int r_expr_0(PRA_Position *p, PRA_Ptree *t);
 int section(PRA_Position *p, PRA_Ptree *t);
 int escape(PRA_Position *p, PRA_Ptree *t);
-int element(PRA_Position *p, PRA_Ptree *t);
-int subscript(PRA_Position *p, PRA_Ptree *t);
+int element(PRA_Position *p, PRA_Ptree *t);//
+int element_separator(PRA_Position *p, PRA_Ptree *t);//
+int subscript(PRA_Position *p, PRA_Ptree *t);//
+
+int digit_2(PRA_Position *p, PRA_Ptree *t){
+	return PRA_oneOf(p, t, PRA_PASS, "01");
+}
+
+int digit_8(PRA_Position *p, PRA_Ptree *t){
+	return PRA_oneOf(p, t, PRA_PASS, "01234567");
+}
 
 int digit_10(PRA_Position *p, PRA_Ptree *t){
 	return PRA_oneOf(p, t, PRA_PASS, "0123456789");
+}
+
+int digit_16(PRA_Position *p, PRA_Ptree *t){
+	return PRA_oneOf(p, t, PRA_PASS, "0123456789abcdefABCDEF");
+}
+
+int digit_64(PRA_Position *p, PRA_Ptree *t){
+	return PRA_oneOf(p, t, PRA_PASS, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+}
+
+int int_2(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "0b"){
+		return 0;
+	}
+	return PRA_repeat(p, t, PRA_PASS, digit_2, 1, 0);
+}
+
+int int_8(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "0o"){
+		return 0;
+	}
+	return PRA_repeat(p, t, PRA_PASS, digit_8, 1, 0);
 }
 
 int int_10(PRA_Position *p, PRA_Ptree *t){
 	return PRA_repeat(p, t, PRA_PASS, digit_10, 1, 0);
 }
 
+int int_16(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "0x"){
+		return 0;
+	}
+	return PRA_repeat(p, t, PRA_PASS, digit_16, 1, 0);
+}
+
+int int_64(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "0s"){
+		return 0;
+	}
+	return PRA_repeat(p, t, PRA_PASS, digit_64, 1, 0);
+}
+
 int integer(PRA_Position *p, PRA_Ptree *t){
+	if(PRA_try(p, t, PRA_ADD, int_2){
+		PRA_setString(t, "(int_2)", 7);
+		return 1;
+	}
+	if(PRA_try(p, t, PRA_ADD, int_8){
+		PRA_setString(t, "(int_8)", 7);
+		return 1;
+	}
+	if(PRA_try(p, t, PRA_ADD, int_16){
+		PRA_setString(t, "(int_16)", 8);
+		return 1;
+	}
+	if(PRA_try(p, t, PRA_ADD, int_64){
+		PRA_setString(t, "(int_64)", 8);
+		return 1;
+	}
 	if(PRA_try(p, t, PRA_ADD, int_10)){
 		PRA_setString(t, "(int_10)", 8);
 		return 1;
@@ -208,6 +273,174 @@ int space(PRA_Position *p, PRA_Ptree *t){
 
 int whitespace_char(PRA_Position *p, PRA_Ptree *t){
 	return PRA_oneOf(p, t, PRA_SKIP, " \t\v");// note: \n and \r\f are newlines and should be considered seperately, and so should indentation
+}
+
+int sigil(PRA_Position *p, PRA_Ptree *t){
+	return PRA_oneOf(p, t, PRA_PASS, "~!$%^&*-+|\\'\":;?/<>,.");
+}
+
+int sigils(PRA_Position *p, PRA_Ptree *t){
+	return PRA_repeat(p, t, PRA_PASS, sigil, 1, 0);
+}
+
+int string(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "\""){
+		return 0;
+	}
+	if(!PRA_repeat(p, t, PRA_PASS, string_char)){
+		return 0;
+	}
+	return PRA_acceptString(p, t, PRA_SKIP, "\"");
+}
+
+int string_char(PRA_Position *p, PRA_Ptree *t){
+	if(PRA_noneOf(p, t, PRA_PASS, "\\\"")){
+		return 1;
+	}
+	return PRA_accept(p, t, PRA_PASS, escape);
+}
+
+int character(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "'"){
+		return 0;
+	}
+	if(!PRA_noneOf(p, t, PRA_PASS, "\\'"){
+		if(!PRA_accept(p, t, PRA_PASS, escape)){
+			return 0;
+		}
+	}
+	return PRA_acceptString(p, t, PRA_SKIP, "'");
+}
+
+int operator(PRA_Position *p, PRA_Ptree *t){
+	if(PRA_accept(p, t, PRA_SKIP, space)){
+		if(!PRA_accept(p, t, PRA_PASS, operatorBesidesSpace)){
+			PRA_setString(t, " ", 1);
+			return 1;
+		}
+		PRA_accept(p, t, PRA_SKIP, space);
+		return 1;
+	}
+	return PRA_accept(p, t, PRA_PASS, operatorBesidesSpace);
+}
+
+int operatorBesidesSpace(PRA_Position *p, PRA_Ptree *t){
+	if(PRA_accept(p, t, PRA_PASS, sigils)){
+		return 1;
+	}
+	if(!PRA_acceptString(p, t, PRA_SKIP, "`"){
+		return 0;
+	}
+	PRA_setString(t, "(infixed_operator)", 18);
+	PRA_accept(p, t, PRA_SKIP, space);
+	if(!PRA_accept(p, t, PRA_ADD, r_expr)){
+		return 0;
+	}
+	PRA_accept(p, t, PRA_SKIP, space);
+	if(!PRA_acceptString(p, t, PRA_SKIP, "`"){
+		return 0;
+	}
+	return 1;
+}
+
+int identifier(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_noneOf(p, t, PRA_PASS, "0123456789`~!$%^&*()-+=|\\{}[]'\":;?/<>,.")){
+		return 0;
+	}
+	PRA_repeat(p, t, PRA_PASS, identifier_char);
+	return 1;
+}
+
+int literal(PRA_Position *p, PRA_Ptree *t){
+	if(PRA_try(p, t, PRA_PASS, integer)){
+		return 1;
+	}
+	if(PRA_accept(p, t, PRA_ADD, decimal)){
+		PRA_setString(t, "(decimal)", 9);
+		return 1;
+	}
+	if(PRA_accept(p, t, PRA_ADD, string)){
+		PRA_setString(t, "(string)", 8);
+		return 1;
+	}
+	if(PRA_accept(p, t, PRA_ADD, character)){
+		PRA_setString(t, "(char)", 6);
+		return 1;
+	}
+	if(PRA_accept(p, t, PRA_ADD, tuple)){
+		PRA_setString(t, "(tuple)", 7);
+		return 1;
+	}
+	if(PRA_accept(p, t, PRA_ADD, dict)){
+		PRA_setString(t, "(dict)", 6):
+		return 1;
+	}
+	return 0;
+}
+
+int tuple(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "(")){
+		return 0;
+	}
+	PRA_accept(p, t, PRA_SKIP, space);
+	if(!PRA_alternate(p, t, PRA_ADD, PRA_SKIP, element, element_seperator)){
+		return 0;
+	}
+	PRA_accept(p, t, PRA_ADD, element);
+	PRA_accept(p, t, PRA_SKIP, space);
+	return PRA_acceptString(p, t, PRA_SKIP, ")");
+}
+
+int dict(PRA_Position *p, PRA_Ptree *t){
+	if(!PRA_acceptString(p, t, PRA_SKIP, "[")){
+		return 0;
+	}
+	PRA_accept(p, t, PRA_SKIP, space);
+	if(!PRA_alternate(p, t, PRA_ADD, PRA_SKIP, element, element_seperator)){
+		return 0;
+	}
+	PRA_accept(p, t, PRA_ADD, element);
+	PRA_accept(p, t, PRA_SKIP, space);
+	return PRA_acceptString(p, t, PRA_SKIP, ")");
+}
+
+int op_c(PRA_Position *p, PRA_Ptree *t){
+	PRA_setString(t, "(op_c)", 6);
+	if(!PRA_accept(p, t, PRA_ADD, operatorBesidesSpace)){
+		return 0;
+	}
+	return PRA_acceptString(p, t, PRA_SKIP, "=");
+}
+
+int element(PRA_Position *p, PRA_Ptree *t){
+	PRA_accept(p, t, PRA_ADD, subscript);
+	return PRA_accept(p, t, PRA_ADD, expr_c);
+}
+
+int element_separator(PRA_Position *p, PRA_Ptree *t){
+	PRA_accept(p, t, PRA_SKIP, space);
+	PRA_acceptString(p, t, PRA_SKIP, ",");
+	PRA_accept(p, t, PRA_SKIP, space);
+}
+
+int subscript(PRA_Position *p, PRA_Ptree *t){
+	PRA_setString(t, "(index)", 7);
+	if(!PRA_acceptString(p, t, PRA_SKIP, "[")){
+		if(!PRA_accept(p, t, PRA_ADD, identifier)){
+			return 0;
+		}
+	}else{
+		PRA_accept(p, t, PRA_SKIP, space);
+		PRA_accept(p, t, PRA_ADD, expr_c);
+		PRA_accept(p, t, PRA_SKIP, space);
+		if(!PRA_acceptString(p, t, PRA_SKIP, "]")){
+			return 0;
+		}
+	}
+	PRA_accept(p, t, PRA_SKIP, space);
+	PRA_acceptString(p, t, PRA_SKIP, "=");
+	PRA_accept(p, t, PRA_SKIP, space);
+	return 1;
 }
 
 
